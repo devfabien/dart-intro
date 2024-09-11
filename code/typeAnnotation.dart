b0f26b49ae84b89b646bccee2db7700917847f15
devfabien <ish001fabi@gmail.com>
2024-09-11 17:00:54 +0200

    feat: create the file for type annotations

---
+++ code/typeAnnotation.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "devfabien"; // or var String name = "devfabien";
+  int age = 25; // const String name = "devfabien";
+  bool isStudent = true; // final String name = "devfabien";
+  print(name);
+  print(age);
+  print(isStudent);
+}
