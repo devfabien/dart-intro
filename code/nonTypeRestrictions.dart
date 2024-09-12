5b243bc470d13515148449dcb8637cc368678253
devfabien <ish001fabi@gmail.com>
2024-09-12 10:03:19 +0200

    feat: add the file for non type restrictions

---
+++ code/nonTypeRestrictions.dart
@@ -0,0 +1,12 @@
+dynamic value = "true";
+value = 100;
+value = "devfabien";
+print(value);
+
+Object element = "true";
+element = 100;
+element = "devfabien";
+print(element);
+
+// both dynamic and Object are used to create variables that can hold any type of value.
+// dynamic is more flexible than Object and is the preferred choice in modern Dart development.
