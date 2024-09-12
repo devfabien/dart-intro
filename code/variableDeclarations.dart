6bd2a52e51e25808f30e1808fce3bb9e79b9cc84
devfabien <ish001fabi@gmail.com>
2024-09-12 10:14:42 +0200

    feat: add the late variables

--- code/variableDeclarations.dart
+++ code/variableDeclarations.dart
@@ -1,3 +1,12 @@
+ // late is used to declare a variable that will be initialized later in the program.
+  // late is useful when you want to initialize a variable after it has been declared.
+  /*
+    Dart often can't determine whether they're set, so it doesn't try.
+    If you're sure that a variable is set before it's used, but Dart disagrees, 
+    you can fix the error by marking the variable as late
+  */
+  late String description = "description";
+
 void main() {
   var name = "John";
   print(name);
@@ -7,4 +16,6 @@ void main() {
   
   final isStudent = true;
   print(isStudent);
+
+  print(description);
 }
