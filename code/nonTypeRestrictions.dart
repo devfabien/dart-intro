1a620284e44e3e17b04c9ea2ad973fc84a64d4f5
devfabien <ish001fabi@gmail.com>
2024-09-17 15:21:37 +0200

    feat: wrapp the codes in the main functions

--- code/nonTypeRestrictions.dart
+++ code/nonTypeRestrictions.dart
@@ -1,12 +1,14 @@
-dynamic value = "true";
-value = 100;
-value = "devfabien";
-print(value);
+void main() {
+  dynamic value = true;
+  value = 100;
+  value = "devfabien";
+  print(value);
 
-Object element = "true";
-element = 100;
-element = "devfabien";
-print(element);
+  Object element = true;
+  element = 100;
+  element = "devfabien";
+  print(element);
+}
 
 // both dynamic and Object are used to create variables that can hold any type of value.
 // dynamic is more flexible than Object and is the preferred choice in modern Dart development.
