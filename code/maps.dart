d338f90c9f5457014458f66baf0eb6e340ca9578
devfabien <ish001fabi@gmail.com>
2024-09-18 10:44:55 +0200

    feat: create the maps in dart

---
+++ code/maps.dart
@@ -0,0 +1,28 @@
+void main() {
+// Here are a couple of simple Dart maps, created using map literals:
+
+  var gifts = {
+    // Key:    Value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 'golden rings'
+  };
+
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  };
+
+// You can create the same objects using a Map constructor
+
+  var gifts2 = Map<String, String>();
+  gifts2['first'] = 'partridge';
+  gifts2['second'] = 'turtledoves';
+  gifts2['fifth'] = 'golden rings';
+
+  var nobleGases2 = Map<int, String>();
+  nobleGases2[2] = 'helium';
+  nobleGases2[10] = 'neon';
+  nobleGases2[18] = 'argon';
+}
